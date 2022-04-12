addfa761ee7bb6f6dbb677def813f17c39ade633
Min Zhu <zhumin@google.com>
2022-04-12 19:13:33 +0000

    cleanup, remove sample code from autoconfig, add package info.

--- src/main/java/com/sample/autoconfig/LanguageAutoConfig.java
+++ src/main/java/com/sample/autoconfig/LanguageAutoConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-// @ConditionalOnClass(LanguageServiceClient.class)
+@ConditionalOnClass(LanguageServiceClient.class)
 @ConditionalOnProperty(value = "spring.cloud.gcp.language.enabled", matchIfMissing = true)
 public class LanguageAutoConfig {
   @Bean
