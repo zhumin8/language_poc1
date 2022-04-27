e7356fe039da2074254cada7ca76f8235682e144
Min Zhu <zhumin@google.com>
2022-04-27 15:44:06 +0000

    remove template, add properties class. adding credentialProvider and projectid to autoconfig.

--- src/main/java/com/sample/autoconfig/LanguageAutoConfig.java
+++ src/main/java/com/sample/autoconfig/LanguageAutoConfig.java
@@ -1,26 +1,59 @@
 package com.sample.autoconfig;
 
+import com.google.api.gax.core.CredentialsProvider;
 import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.LanguageServiceSettings;
+import com.google.cloud.spring.autoconfigure.vision.CloudVisionAutoConfiguration;
+import com.google.cloud.spring.core.DefaultCredentialsProvider;
+import com.google.cloud.spring.core.GcpProjectIdProvider;
+import com.google.cloud.spring.core.UserAgentHeaderProvider;
 import java.io.IOException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(LanguageServiceClient.class)
 @ConditionalOnProperty(value = "spring.cloud.gcp.language.enabled", matchIfMissing = true)
+@EnableConfigurationProperties(LanguageProperties.class)
 public class LanguageAutoConfig {
-  @Bean
-  @ConditionalOnMissingBean
-  public LanguageServiceClient languageServiceClient() throws IOException {
-     return LanguageServiceClient.create();
+
+  private final LanguageProperties clientProperties;
+  private final CredentialsProvider credentialsProvider;
+  private final String projectId;
+
+  public LanguageAutoConfig(LanguageProperties properties,
+      CredentialsProvider credentialsProvider,
+      GcpProjectIdProvider projectIdProvider)
+      throws IOException {
+    this.clientProperties = properties;
+    if (properties.getCredentials().hasKey()) {
+      this.credentialsProvider = new DefaultCredentialsProvider(properties);
+    } else {
+      this.credentialsProvider = credentialsProvider;
+    }
+    if (properties.getProjectId() != null) {
+      this.projectId =
+          properties.getProjectId();
+    } else {
+      this.projectId =
+          projectIdProvider.getProjectId();
+    }
   }
 
   @Bean
   @ConditionalOnMissingBean
-  public LanguageTemplate cloudVisionTemplate(LanguageServiceClient languageServiceClient) {
-    return new LanguageTemplate(languageServiceClient);
+  public LanguageServiceClient languageServiceClient() throws IOException {
+
+    LanguageServiceSettings clientSettings =
+        LanguageServiceSettings.newBuilder()
+            .setCredentialsProvider(this.credentialsProvider)
+            .setQuotaProjectId(this.projectId)
+            // .setEndpoint("language.googleapis.com:443")
+        .build();
+     return LanguageServiceClient.create(clientSettings);
   }
 }
--- src/main/java/com/sample/autoconfig/LanguageProperties.java
+++ src/main/java/com/sample/autoconfig/LanguageProperties.java
@@ -1,2 +1,29 @@
-package com.sample.autoconfig;public class LanguageProperties {
+package com.sample.autoconfig;
+
+import com.google.cloud.spring.core.Credentials;
+import com.google.cloud.spring.core.CredentialsSupplier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+@ConfigurationProperties("spring.cloud.gcp.language")
+public class LanguageProperties implements CredentialsSupplier {
+
+  // Overrides the GCP OAuth2 credentials specified in the Core module.
+  @NestedConfigurationProperty
+  private final Credentials credentials = new Credentials("https://www.googleapis.com/auth/cloud-language");
+
+  private String projectId;
+
+  @Override
+  public Credentials getCredentials() {
+    return this.credentials;
+  }
+
+  public String getProjectId() {
+    return projectId;
+  }
+
+  public void setProjectId(String projectId) {
+    this.projectId = projectId;
+  }
 }
